436a8e824e5e07a04565518562933fee8258c0e7
iandsilva4 <iandsilva4@gmail.com>
2025-09-22 13:03:35 -0400

    Add ChatGPT Product Strategy presentation card; link Google Doc; fix TS typings; add .gitignore

---
+++ websitev2/astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  site: process.env.URL || 'https://iandsilva4.github.io',
+  base: '/',
+  integrations: [sitemap()]
+});
